88b00bf7585aa0aeefc5074267a602a17830ccc9
WimukthiPWijerathne <wijerathne_gpwp_e23@engug.ruh.ac.lk>
2025-01-03 00:01:58 +0530

    add tailwind

---
+++ dashboard/src/assets/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <>
+            <h1 className="text-navy text-2xl">TRANSMISSION LINE FAULT DETECTION SYSTEM</h1>
+        </>
+    );
+}
